8f552c384c40ace8ba0c3641f8e8aef015559de0
mmalexei <asmm.cel2018@gmail.com>
2023-09-12 00:58:44 -0500

    Practica 3 Alumnos
    Pagina de alumnos INPUT, OUTPUT Event Emiter, comunicacion entre componentes hijo y padre

---
+++ ionic-basic/src/app/componentes/datos-alumno/datos-alumno.component.ts
@@ -0,0 +1,19 @@
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { Alumno } from 'src/app/interface/alumno';
+
+@Component({
+  selector: 'app-datos-alumno',
+  templateUrl: './datos-alumno.component.html',
+  styleUrls: ['./datos-alumno.component.scss'],
+})
+export class DatosAlumnoComponent  implements OnInit {
+
+  @Input() alumnosList:Alumno[] =[];
+  @Output() eliminar: any = new EventEmitter<Alumno[]>();
+  @Output() editar:  any = new EventEmitter<number>();
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
